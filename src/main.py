d769e3fe22b9c4de02b31ccfbbd942addb5f02b4
Iimen <intern@DESKTOP-MB1S1QV.localdomain>
2026-09-09 12:01:57 +0900

    メインの修正

--- src/main.py
+++ src/main.py
@@ -3,17 +3,37 @@ import streamlit as st
 
 def main():
     # ページ設定
-    st.set_page_config(page_title="画像アップロードアプリ")
+    st.set_page_config(page_title="旅行精算アプリ")
+
     pages = st.navigation(
         [
-            st.Page("pages/travel.py",title="旅行"),
-            st.Page("pages/home.py", title="レシート登録"),
-            st.Page("pages/search.py",title="検索"),
-            st.Page("pages/settlement.py",title="割り勘"),
+            st.Page(
+                "pages/travel.py",
+                title="旅行",
+                url_path="travel",
+                default=True
+            ),
+            st.Page(
+                "pages/home.py",
+                title="レシート登録",
+                url_path="home"
+            ),
+            st.Page(
+                "pages/search.py",
+                title="検索",
+                url_path="search"
+            ),
+            st.Page(
+                "pages/settlement.py",
+                title="割り勘",
+                url_path="settlement"
+            ),
         ]
     )
+
     pages.run()
 
 
 if __name__ == "__main__":
     main()
+
