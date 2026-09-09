735a1a6389a21b7d97a80d454f2b15724b4545e1
take <intern@DESKTOP-3SEF63O.localdomain>
2026-09-09 10:23:07 +0900

    init.sqlの改良

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,20 @@
 CREATE TABLE mydb.evidences (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    text TEXT
-)
+    text TEXT,
+    image_path VARCHAR(255),
+    travel_id INT,
+    payer_id INT,
+    amount INT
+);
+
+CREATE TABLE mydb.travels (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE mydb.members (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    travel_id INT,
+    name VARCHAR(255)
+);
